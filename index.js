3e8a40a8a8d7f9062825c2d8a23a5e74e23e29f1
unknown <fernandoopaez@gmail.com>
2022-10-28 20:07:15 -0300

    commit scrollReveal

--- index.js
+++ index.js
@@ -1,4 +1,107 @@
-document.querySelector(".menu-btn").addEventListener('click', ()=>{
+
+/* Menu responsive */
+document.querySelector(".menu-btn").addEventListener('click', () => {
     document.querySelector(".nav-menu").classList.toggle("show");
 
 })
+/* Animaciones */
+ScrollReveal().reveal('.nav-main', { duration: 3000 });
+ScrollReveal().reveal('.col1presentacion', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '100px',
+});
+ScrollReveal().reveal('.col2presentacion', {
+    duration: 4000,
+    origin: 'right',
+    distance: '300px',
+});
+
+ScrollReveal().reveal('.servicios', {
+    duration: 3000,
+});
+
+ScrollReveal().reveal('.consulta', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '300px',
+});
+
+ScrollReveal().reveal('.fondo2', {
+    duration: 3000,
+    origin: 'right',
+    distance: '300px',
+});
+
+ScrollReveal().reveal('.recuadrocuadrado', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '300px',
+});
+
+ScrollReveal().reveal('.cuadroinferior', {
+    duration: 4000,
+    origin: 'bottom',
+    distance: '300px',
+
+});
+
+ScrollReveal().reveal('.imgestadisticas', {
+    duration: 4000,
+    origin: 'right',
+    distance: '300px',
+});
+
+ScrollReveal().reveal('.col1estadisticas', {
+    duration: 4000,
+    
+});
+
+ScrollReveal().reveal('.recformulario', {
+    duration: 4000,
+    
+});
+
+ScrollReveal().reveal('.footer', {
+    duration: 4000,
+    origin: 'bottom',
+    distance: '300px',
+
+});
+
+ScrollReveal().reveal('.marcointroduccion', {
+    duration: 4000,
+    
+});
+
+
+ScrollReveal().reveal('.planes', {
+    duration: 4000,
+    origin: 'bottom',
+    distance: '300px',
+
+});
+
+ScrollReveal().reveal('.marcopagos', {
+    duration: 4000,
+    origin: 'left',
+    distance: '300px',
+
+});
+
+ScrollReveal().reveal('.fondopagos', {
+    duration: 4000,
+    origin: 'right',
+    distance: '300px',
+
+});
+ScrollReveal().reveal('.transferencias', {
+    duration: 5000,
+    origin: 'bottom',
+    distance: '300px',
+    
+
+});
+
+
+
